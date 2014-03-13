5affe4764d74210afd7a97b8d2c342d54712b2f0
Fernando Honig <fernando.honig@rackspace.co.uk>
2014-03-13 13:23:37 +0000

    Added new checks for rspec tests

--- spec/_server_rhel_spec.rb
+++
@@ -1,6 +0,0 @@
-require 'spec_helper'
-
-describe 'rackspace_mysql::_server_rhel.rb' do
-  let(:centos6_run) { ChefSpec::Runner.new(platform: 'centos', version: '6.4').converge(described_recipe) }
-  let(:ubuntu_1204_run) { ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04').converge(described_recipe) }
-end
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -1,30 +1,51 @@
 require 'spec_helper'
 
+describe 'rackspace_mysql::_server_rhel.rb' do
+  let(:centos6_run) { ChefSpec::Runner.new(platform: 'centos', version: '6.4').converge(described_recipe) }
+end
+
 describe 'rackspace_mysql::server' do
-  let(:centos6_run) do
-    ChefSpec::Runner.new(platform: 'centos', version: '6.4') do |node|
-      node.set['rackspace_mysql']['server_debian_password'] = 'idontlikerandompasswords'
-      node.set['rackspace_mysql']['server_root_password'] = 'idontlikerandompasswords'
-      node.set['rackspace_mysql']['server_repl_password'] = 'idontlikerandompasswords'
-    end.converge(described_recipe)
-  end
-  let(:ubuntu_1204_run) do
+  let(:chef_run) do
     ChefSpec::Runner.new(platform: 'ubuntu', version: '12.04') do |node|
       node.set['rackspace_mysql']['server_debian_password'] = 'idontlikerandompasswords'
       node.set['rackspace_mysql']['server_root_password'] = 'idontlikerandompasswords'
       node.set['rackspace_mysql']['server_repl_password'] = 'idontlikerandompasswords'
     end.converge(described_recipe)
   end
-
-  before do
-    stub_command("/usr/bin/mysql -u root -e 'show databases;'").and_return(true)
+  it 'include the default recipe' do
+    expect(chef_run).to include_recipe 'rackspace_mysql::_server_debian'
   end
-
-  it 'includes _server_rhel on centos6' do
-    expect(centos6_run).to include_recipe('rackspace_mysql::_server_rhel')
+  it 'populate the /var/cache/local/preseeding directory' do
+    expect(chef_run).to create_directory('/var/cache/local/preseeding')
   end
-
-  it 'includes _server_debian on ubuntu1204' do
-    expect(ubuntu_1204_run).to include_recipe('rackspace_mysql::_server_debian')
+  it 'populate the /var/run/mysqld directory' do
+    expect(chef_run).to create_directory('/var/run/mysqld')
+  end
+  it 'populate the /var/lib/mysqltmp directory' do
+    expect(chef_run).to create_directory('/var/lib/mysqltmp')
+  end
+  it 'populate the /etc/mysql/conf.d directory' do
+    expect(chef_run).to create_directory('/etc/mysql/conf.d')
+  end
+  it 'populate the /var/lib/mysql directory' do
+    expect(chef_run).to create_directory('/var/lib/mysql')
+  end
+  it 'template mysql-server.seed is created' do
+    expect(chef_run).to create_template '/var/cache/local/preseeding/mysql-server.seed'
+  end
+  it 'template /etc/mysql/debian.cnf is created' do
+    expect(chef_run).to create_template '/etc/mysql/debian.cnf'
+  end
+  it 'template /etc/init/mysql.conf is created' do
+    expect(chef_run).to create_template '/etc/init/mysql.conf'
+  end
+  it 'template /etc/apparmor.d/usr.sbin.mysqld is created' do
+    expect(chef_run).to create_template '/etc/apparmor.d/usr.sbin.mysqld'
+  end
+  it 'installs package mysql-server' do
+    expect(chef_run).to install_package('mysql-server')
+  end
+  it 'installs package apparmor-utils' do
+    expect(chef_run).to install_package('apparmor-utils')
   end
 end
